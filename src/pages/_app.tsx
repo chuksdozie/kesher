caa8015557a5d4f8f0bfb17f2e5ec80faa4f5a2e
EmperorPISO <princeokechukwu83@gmail.com>
2025-08-22 18:12:53 +0100

    feat: dashboard layout setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import DashboardLayout from "@/layout/DashboardLayout"; 
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
+      <DashboardLayout>
       <Component {...pageProps} />
+      </DashboardLayout>
     </QueryClientProvider>
   );
 }
