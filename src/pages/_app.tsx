6f21de4c9e557897e8aee85138810509853ae26a
EmperorPISO <princeokechukwu83@gmail.com>
2025-08-22 13:53:29 +0100

    pushed updated api.ts and _app.tsx files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+// pages/_app.tsx
+import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from '@/hooks/App';
+import { useState } from "react";
 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
