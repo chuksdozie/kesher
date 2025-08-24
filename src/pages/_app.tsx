eca154afa424e4b6088bd9a70070f86329c2777c
chuks dozie <chuksdozie48@gmail.com>
2025-08-24 17:06:28 +0100

    Fonts: Setup...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,10 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import { Inter, Karla } from "next/font/google";
+import { Outfit } from "next/font/google";
 import DashboardLayout from "@/layout/DashboardLayout";
 
-const inter = Karla({ subsets: ["latin"] });
+const inter = Outfit({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
