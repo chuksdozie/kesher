4e8948a2c3f0fbf17fd5bdcb4bddc7c99ebcb04c
chuks dozie <chuksdozie48@gmail.com>
2025-08-24 17:56:20 +0100

    Feat: Tailwind Setup and Sample Color Scheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 // pages/_app.tsx
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
--- src/pages/text.tsx
+++ src/pages/text.tsx
@@ -1,17 +1,33 @@
-import useSheet from '@/hooks/Sheet';
-import React from 'react';
-type nameType = {Users: string}
+import useSheet from "@/hooks/Sheet";
+import React from "react";
+type nameType = { Users: string };
 export default function Text() {
-
-    const [text, setText] = React.useState("");
-    const {names, isLoading, error, addName} = useSheet();
-  return <div>Text Page
-    <input type="text" value={text} onChange={(e)=>{setText(e.target.value)}} />
-    <button onClick={()=>{
-        addName(text);
-    }}>Do do</button>
-    {isLoading && <div>Loading...</div>}
-    {error && <div>Error: {(error as Error).message}</div>}
-    {names.map((name: nameType, index: number) => (
-        <div key={index}>{name.Users}</div> ) )}
-  </div> }
+  const [text, setText] = React.useState("");
+  const { names, isLoading, error, addName } = useSheet();
+  return (
+    <div>
+      Text Page
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          addName(text);
+        }}
+      >
+        Do do
+      </button>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {(error as Error).message}</div>}
+      {names.map((name: nameType, index: number) => (
+        <div key={index} className="bg-amber-500 my-1">
+          {name.Users}
+        </div>
+      ))}
+    </div>
+  );
+}
