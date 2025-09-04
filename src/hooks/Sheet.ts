a03474a1eaeef767cf062e2b5dbe45402ac9786d
EmperorPISO <princeokechukwu83@gmail.com>
2025-09-04 10:35:20 +0100

    fix: modified hooks folder to include index.ts file where all hooks are imported for easy importation throughout the project

--- src/hooks/Sheet.ts
+++ src/hooks/Sheet.ts
@@ -1,33 +1,42 @@
-import { useQuery, useMutation, useQueryClient} from "@tanstack/react-query"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { API } from "@/api/api";
 
 const getNames = async () => {
-    const response = await API.get('/exec?path=Sheet1&action=read');
-    return response.data;}
+  const response = await API.get("/exec?path=Sheet1&action=read");
+  return response.data;
+};
 
-const addName =  (name: string) => {
-    return API.get(`/exec?path=Sheet1&action=write&Users=${name}` );
-    }
+const addName = (name: string) => {
+  return API.get(`/exec?path=Sheet1&action=write&Users=${name}`);
+};
 
+export function useSheet() {
+  const queryClient = useQueryClient();
 
-export default function useSheet() {
-    const queryClient = useQueryClient();
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["names"],
+    queryFn: getNames,
+  });
+  // const { data: users , isLoading, error } = useQuery({
+  //     queryKey: ['names'],
+  //     queryFn: getNames,
+  // });
 
-    const { data: users , isLoading, error } = useQuery({
-        queryKey: ['names'],
-        queryFn: getNames,
-    });
-    // const { data: users , isLoading, error } = useQuery({
-    //     queryKey: ['names'],
-    //     queryFn: getNames,
-    // });
+  const mutation = useMutation({
+    mutationFn: addName,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["names"] });
+    },
+  });
 
-    const mutation = useMutation({
-        mutationFn: addName,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['names'] });
-        },
-    });
-
-    return {names: users?.data || [], isLoading: mutation.isPending, error: mutation.error, addName: mutation.mutate};
+  return {
+    names: users?.data || [],
+    isLoading: mutation.isPending,
+    error: mutation.error,
+    addName: mutation.mutate,
+  };
 }
